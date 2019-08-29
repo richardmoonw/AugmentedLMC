af516357efb7b521d86d436b2d757f699c846854
Ricardo <r_luna99@hotmail.com>
2019-08-29 15:44:33 -0600

    Solo faltan los cambios finales

--- src/LMC.js
+++ src/LMC.js
@@ -11,84 +11,106 @@ function fill_data(){
     
 }
 
+function reset(){
+    for(var i=0; i<100; i++){
+        document.getElementById("input" + i).value = "000";
+    }
+    document.getElementById("input100").value = "000";
+    document.getElementById("input102").value = "000";
+    document.getElementById("input103").value = "000";
+}
+
+var step_calculator = document.getElementById("input100");
+var step_pc = document.getElementById("input103");
+var step_output = document.getElementById("input102");
+var step_interrupt = document.getElementById("interrupt");
+var step_instruction = "1";
+var step_mailbox = "";
 var step_counter = 0;
+var temp_counter = 0;
+
 
 function step_by_step(){
+    if(step_interrupt.checked == true){
+        temp_counter = step_counter;
+        step_counter = 60;
+        step_interrupt.checked = false;
+    }
+
     if(flag == 0){
         fill_data();
         flag = 1;
     }
-    
-    let calculator = document.getElementById("input100");
-    let pc = document.getElementById("input103");
-    let output = document.getElementById("input102");
-    let instruction = "";
-    let mailbox = "";
 
+    if(step_instruction[0] != "0"){
+        step_pc.value = step_counter;
+        step_instruction = document.getElementById("input" + step_counter).value;
+        step_mailbox = step_instruction[1] + step_instruction[2];
 
-    while(instruction[0] != "0"){
-        pc.value = step_counter;
-        instruction = document.getElementById("input" + step_counter).value;
-        mailbox = instruction[1] + instruction[2];
-
-        if(calculator.value >= 0){
+        if(step_calculator.value >= 0 && step_calculator.value != ""){
             check();
         }
         else {
             uncheck();
         }
-        if(calculator.value == 0){
+        if(step_calculator.value == 0 && step_calculator.value != ""){
             check2();
         }
         else {
             uncheck2();
         }
-        switch (instruction[0]){
+
+        switch (step_instruction[0]){
             case "1":
-                calculator.value = parseInt(calculator.value) + parseInt(boxes[parseInt(mailbox)]);
+                step_calculator.value = parseInt(step_calculator.value) + parseInt(boxes[parseInt(step_mailbox)]);
                 break;
             case "2":
-                calculator.value = parseInt(calculator.value) - parseInt(boxes[parseInt(mailbox)]);
+                step_calculator.value = parseInt(step_calculator.value) - parseInt(boxes[parseInt(step_mailbox)]);
                 break;
             case "3":
-                boxes[parseInt(mailbox)] = calculator.value;
-                document.getElementById("input"+mailbox).value = boxes[parseInt(mailbox)];
+                boxes[parseInt(step_mailbox)] = step_calculator.value;
+                document.getElementById("input"+step_mailbox).value = boxes[parseInt(step_mailbox)];
                 break;
             case "5":
-                calculator.value = boxes[parseInt(mailbox)];
+                step_calculator.value = boxes[parseInt(step_mailbox)];
                 break;
             case "6":
-                program_counter = parseInt(mailbox) - 1;
+                step_counter = parseInt(step_mailbox) - 1;
                 break;
             case "7":
                 if(document.getElementById("zero").checked){
-                    program_counter = parseInt(mailbox) - 1;
+                    step_counter = parseInt(step_mailbox) - 1;
                 }
                 break;
             case "8":
                 if(document.getElementById("possitive").checked){
-                    program_counter = parseInt(mailbox) - 1;
+                    step__counter = parseInt(step_mailbox) - 1;
                 }
                 break;
             case "9":
-                if (instruction[2] == "1"){
+                if (step_instruction[2] == "1"){
                     var value = prompt("Enter a number:");
                     if(value == null){
                         value = 0;
                     }
-                    calculator.value = value;
+                    step_calculator.value = value;
                 }
 
-                else if(instruction[2] == "2"){
-                    output.value = calculator.value;
+                else if(step_instruction[2] == "2"){
+                    step_output.value = step_calculator.value;
+                }
+
+                else if(step_instruction[1] == "9"){
+                    step_counter = temp_counter;
                 }
                 break;
                 
             default: 
                 break;
         }
+        step_counter++;
     }
-    step_counter++;
+
 }
 
 
@@ -107,13 +129,13 @@ function execute(){
         instruction = document.getElementById("input" + program_counter).value;
         mailbox = instruction[1] + instruction[2];
 
-        if(calculator.value >= 0){
+        if(calculator.value >= 0 && step_calculator.value != ""){
             check();
         }
         else {
             uncheck();
         }
-        if(calculator.value == 0){
+        if(calculator.value == 0 && step_calculator.value != ""){
             check2();
         }
         else {
